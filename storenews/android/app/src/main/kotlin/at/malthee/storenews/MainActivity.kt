ffc93f32a92cbd583d706c6e48cf73284eb07dd9
Marcel Salvenmoser <18032233+malthee@users.noreply.github.com>
2023-05-22 20:47:30 +0200

    Setup beacon plugin

--- storenews/android/app/src/main/kotlin/at/malthee/storenews/MainActivity.kt
+++ storenews/android/app/src/main/kotlin/at/malthee/storenews/MainActivity.kt
@@ -1,6 +1,20 @@
 package at.malthee.storenews
 
 import io.flutter.embedding.android.FlutterActivity
+import com.umair.beacons_plugin.BeaconsPlugin
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onPause() {
+        super.onPause()
+
+        //Start Background service to scan BLE devices
+        BeaconsPlugin.startBackgroundService(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //Stop Background service, app is in foreground
+        BeaconsPlugin.stopBackgroundService(this)
+    }
 }
